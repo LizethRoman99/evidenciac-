9aa9d70091925fb2bc9926cd8f7e9139a8567169
LizethRoman99 <134404192+LizethRoman99@users.noreply.github.com>
2024-03-07 15:06:11 -0500

    se actualizo el crud mvc y corregi razor

--- CRUDMVC5/CRUDMVC5/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CRUDMVC5.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
